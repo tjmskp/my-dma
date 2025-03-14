67fa5a41bc2f45cc086cde709703d77c98fe8f0a
tjmskp <017832jd@gmail.com>
2025-03-14 23:57:26 +0600

    Initial commit: DMA application with campaign management features

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const config = {
+  reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+}
+
+export default config 
